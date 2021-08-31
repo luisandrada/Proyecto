58db708b8824eb9f00c16a3c487246752e863fef
luisandrada <luisandradaok@gmail.com>
2021-08-31 15:11:16 -0300

    ordenado precio relevancia

--- js/products.js
+++ js/products.js
@@ -1,30 +1,101 @@
-const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
-fetch (PRODUCTS_URL)
-.then(respuesta => respuesta.json())
-.then(auto => {
+var arrayproducto=[];
 
+function showProductsList(auto){
+
+    let htmlContentToAppend = "";
     for(vehiculos of auto){
-    document.getElementById("cont").innerHTML+=`
-    <div class="list-group-item list-group-item-action">
-        <div class="row">
-            <div class="col-3">
-                <img src="` + vehiculos.imgSrc + `" alt="` + `" class="img-thumbnail">
-            </div>
-            
-            <div class="col">
-            
-                <div class="d-flex w-100 justify-content-between">
-                
-                    <h4 class="mb-1">`+ vehiculos.name +`</h4>
+        
 
-<p>${vehiculos.description}</p>
-
-                   
+            htmlContentToAppend += `<div class="list-group-item list-group-item-action ">
+            <div class="row" id="coso">
+                <div class="col-3">
+                    <img src="` + vehiculos.imgSrc + `" alt="` + vehiculos.desc + `" class="img-thumbnail">
                 </div>
-
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ vehiculos.name +`-`+vehiculos.cost + vehiculos.currency+`</h4>
+                        <small class="text-muted">` + vehiculos.soldCount + ` artículos</small>
+                    </div>
+                    <div class="d-flex w-100 justify-content-between">
+                        <p class="mb-1">`+ vehiculos.description +`</p>
+                    </div>
+                </div>
             </div>
-            <div class="text-muted">` +"Precio: " +vehiculos.currency + vehiculos.cost + ` </div>
         </div>
-    </div>
-    `
-}})
+        `
+        }
+
+        document.getElementById("cont").innerHTML = htmlContentToAppend;
+      
+    }
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(PRODUCTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok"){
+                arrayproducto=resultObj.data;
+                
+                
+                showProductsList(arrayproducto);
+            }
+
+            document.getElementById("precioalto").addEventListener("click",()=>{
+                let vehiculos_ordenado_precio_descendente = arrayproducto.sort((em1,em2)=>{
+                    return em2.cost - em1.cost
+                    
+                    })
+                showProductsList(vehiculos_ordenado_precio_descendente);
+            }
+
+            );
+            document.getElementById("preciobajo").addEventListener("click",()=>{
+                let vehiculos_ordenado_precio_ascendente = arrayproducto.sort((em1,em2)=>{
+                    return em1.cost - em2.cost
+                    
+                    })
+                showProductsList(vehiculos_ordenado_precio_ascendente);
+            }
+
+            );
+
+document.getElementById("relevanciabajo").addEventListener("click",()=>{
+    let vehiculos_ordenado_relevancia_ascendente = arrayproducto.sort((em1,em2)=>{
+        return em1.soldCount - em2.soldCount
+        
+        })
+    showProductsList(vehiculos_ordenado_relevancia_ascendente);
+}
+
+);
+document.getElementById("relevanciaalto").addEventListener("click",()=>{
+    let vehiculos_ordenado_relevancia_descendente = arrayproducto.sort((em1,em2)=>{
+        return em2.soldCount - em1.soldCount
+        
+        })
+    showProductsList(vehiculos_ordenado_relevancia_descendente);
+}
+
+);
+
+    })
+   
+
+    
+})
+
+
+       
+
+  
+
+
+
+
+
+
+
+
+    
+   
+
+
+
+    
